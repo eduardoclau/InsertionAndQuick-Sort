2dd25a63859e18b0acfbc537fb0c1bc0bdd890c1
Eduardo Claudino de Carvalho <105742162+eduardoclau@users.noreply.github.com>
2023-06-11 16:50:22 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+# Insertion Sort
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+
+# Exemplo de uso:
+arr = [5, 2, 4, 6, 1, 3]
+insertion_sort(arr)
+print(arr)
+
+
+# Quick Sort
+
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+
+def quick_sort(arr, low, high):
+    if low < high:
+        pivot_index = partition(arr, low, high)
+        quick_sort(arr, low, pivot_index - 1)
+        quick_sort(arr, pivot_index + 1, high)
+
+
+# Exemplo de uso:
+arr = [5, 2, 4, 6, 1, 3, 7]
+quick_sort(arr, 0, len(arr) - 1)
+print(arr)
